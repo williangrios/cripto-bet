10f784de877b1e33d041434889f364fad8215a2f
Willian Goncalves Rios <williangrios@yahoo.com.br>
2023-12-18 19:04:04 -0300

    style: styles improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex h-[calc(100vh_-_290px)] py-10 flex-col items-center text-lg text-text-darktheme space-y-3">
-      <h1 className="text-3xl font-bold"><span className="text-secondary-medium">Cripto</span><span className="text-primary-medium">Bet</span></h1>
-      <p>Estamos construindo algo incrível para você!</p>
-      <p>Em breve, ofereceremos uma experiência única de apostas esportivas com criptomoedas.</p>
-      <p>Aguarde ansiosamente por nossas emocionantes opções de aposta em tecnologia blockchain, para dar mais segurança a você.</p>
-      <p>Enquanto isso, explore o fascinante mundo das criptomoedas e fique ligado para as novidades!</p>
+    <main className="flex h-[calc(100vh_-_290px)] py-10 flex-col items-center text-lg">
+      <div className="flex items-center flex-col justify-center h-full space-y-3">
+        <p>Estamos construindo algo incrível para você!</p>
+        <p>Em breve, ofereceremos uma experiência única de apostas esportivas com criptomoedas.</p>
+        <p>Aguarde ansiosamente por nossas emocionantes opções de aposta em tecnologia blockchain, para dar mais segurança a você.</p>
+        <p>Enquanto isso, explore o fascinante mundo das criptomoedas e fique ligado para as novidades!!!</p>
+      </div>
     </main>
 
   )
